f77450d3f2cf65d8fcee14240db373caa1a1f77f
Extasio15 <popodj15@gmail.com>
2021-09-21 21:23:21 +0200

    Main file structuration

---
+++ src/main/java/model/Vertex.java
@@ -0,0 +1,11 @@
+package model;
+
+import javafx.geometry.Point3D;
+
+public class Vertex extends Point3D {
+
+
+    public Vertex(double v, double v1, double v2) {
+        super(v, v1, v2);
+    }
+}
