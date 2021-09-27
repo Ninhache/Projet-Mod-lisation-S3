550373acc15bd187241a2add1dcb0872941094bd
ALMEIDA Néo <neo.almeida.etu@univ-lille.fr>
2021-09-27 10:42:08 +0200

    Remove folders | fix vertex

--- src/main/java/model/Vertex.java
+++ src/main/java/model/Vertex.java
@@ -5,7 +5,7 @@ import javafx.geometry.Point3D;
 public class Vertex extends Point3D {
 
 
-    public Vertex(double v, double v1, double v2) {
-        super(v, v1, v2);
+    public Vertex(double x, double y, double z) {
+        super(x, y, z);
     }
 }
