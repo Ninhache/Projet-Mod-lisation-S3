9f5e98a83aa60cf70abf8b8154b62b8c6dbd173e
Néo Almeida <neo.almeida.etu@univ-lille.fr>
2021-10-04 02:27:02 +0200

    FICHIERS DE PAUL

---
+++ src/main/java/model/Matrix.java
@@ -0,0 +1,101 @@
+package model;
+
+import java.util.Arrays;
+
+public class Matrix {
+
+    private double[][] values;
+
+    public Matrix(double[][] values) {
+        this.values = values;
+    }
+
+    public Matrix multiplyMatrix(Matrix o) {
+        if (canMultiply(o)) {
+            double[][] res = new double[this.getRowCount()][o.getColumnCount()];
+            for (int row = 0; row < res.length; row++) {
+                for (int col = 0; col < res[row].length; col++) {
+                    System.out.println(row +" "+ col);
+                    //res[row][col] = multiplyMatricesCell(this, o, row, col);
+                }
+            }
+            return new Matrix(res);
+        } else {
+            throw new IllegalArgumentException("MatrixNotMultipliable");
+        }
+    }
+
+    public Matrix sumMatrix(Matrix o) {
+        if(canSum(o)) {
+            double[][] res = new double[this.getRowCount()][this.getColumnCount()];
+            for (int row = 0; row <  this.getRowCount(); row++) {
+                for (int col = 0; col < this.getColumnCount(); col++) {
+                    res[row][col] = this.getValues()[row][col] + o.getValues()[row][col];
+                }
+            }
+            return new Matrix(res);
+        }else {
+            throw new IllegalArgumentException("MatrixNotSummable");
+        }
+
+    }
+
+    public double multiplyMatricesCell(Matrix matrix, Matrix o, int row, int col) {
+        double cell = 0;
+        for (int i = 0; i < values.length; i++) {
+            cell += matrix.getValues()[row][i] * o.getValues()[i][col];
+        }
+        return cell;
+    }
+
+    public boolean canMultiply(Matrix o) {
+        if (this.getRowCount() == o.getColumnCount()) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean canSum(Matrix o) {
+        if(this.getRowCount() == o.getRowCount() && this.getColumnCount() == o.getColumnCount()) {
+            return true;
+        }
+        return false;
+    }
+    public int getRowCount() {
+        return values.length;
+    }
+
+    public int getColumnCount() {
+        return values[1].length;
+    }
+
+    public double[][] getValues() {
+        return values;
+    }
+
+    public void setValues(double[][] values) {
+        this.values = values;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sbResult = new StringBuilder();
+
+        for(int i = 0; i < getRowCount();i++)
+        {
+            for(int j = 0; j < getColumnCount();j++)
+            {
+                sbResult.append(getValues()[i][j]);
+                sbResult.append("\t");
+
+                if(i == getRowCount() && j == getColumnCount())
+                {
+                    sbResult.append("\n");
+                }
+            }
+        }
+
+        return sbResult.toString();
+    }
+
+}
---
+++ src/main/java/model/tmp.java
@@ -0,0 +1,22 @@
+package model;
+
+public class tmp {
+
+    public static void main(String[] args) {
+
+        System.out.println("youpi");
+        Matrix m1 = new Matrix(new double[][] {{1,1,1},{3,0,2}});
+        Matrix m2 = new Matrix(new double[][] {{0,7,2},{0,5,1}});
+        System.out.println(m1.canMultiply(m2));
+        System.out.println(m1.canSum(m2));
+        Matrix m3 = m1.sumMatrix(m2);
+        System.out.println(m3);
+        Matrix m4 = new Matrix(new double[][] {{1d,5d},{2d,3d},{1d,7d}});
+        Matrix m5 = new Matrix(new double[][] {{1d, 2d, 3d},{5d, 2d, 8d}});
+        System.out.println(m4.canMultiply(m5));
+        Matrix m6 = m4.multiplyMatrix(m5);
+        System.out.println(m6);
+    }
+
+}
+
