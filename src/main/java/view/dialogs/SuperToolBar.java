9f64269bc3aca1b9bc2600a26c31d4c90c7df070
Néo Almeida <neo.almeida.etu@univ-lille.fr>
2021-10-06 19:21:29 +0200

    Save img done

--- src/main/java/view/dialogs/SuperToolBar.java
+++ src/main/java/view/dialogs/SuperToolBar.java
@@ -6,14 +6,22 @@ import javafx.event.ActionEvent;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.*;
+import javafx.scene.image.WritableImage;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 
+import java.awt.image.RenderedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Objects;
 import java.util.Scanner;
+import java.util.logging.Logger;
+
+import javafx.embed.swing.SwingFXUtils;
+
+import javax.imageio.ImageIO;
 
 public class SuperToolBar extends MenuBar {
 
@@ -153,6 +161,7 @@ public class SuperToolBar extends MenuBar {
         System.out.println(file.getPath());
     }
 
+
     private void openFiles(String path) {
         System.out.println("ON OUVRE CA : ["+path+"]");
     }
@@ -163,6 +172,28 @@ public class SuperToolBar extends MenuBar {
         gc.setFill(Color.LIGHTBLUE);
         gc.fillRect(0,0,canvas.getHeight(), canvas.getWidth());
         gc.fill();
+
+
+        fileChooser = new FileChooser();
+
+        //Set extension filter
+        FileChooser.ExtensionFilter extFilter =
+                new FileChooser.ExtensionFilter("png files (*.png)", "*.png");
+        fileChooser.getExtensionFilters().add(extFilter);
+
+        //Show save file dialog
+        File file = fileChooser.showSaveDialog(this.getParent().getScene().getWindow());
+
+        if(file != null){
+            try {
+                WritableImage writableImage = new WritableImage((int) canvas.getWidth(), (int)canvas.getHeight());
+                canvas.snapshot(null, writableImage);
+                RenderedImage renderedImage = SwingFXUtils.fromFXImage(writableImage, null);
+                ImageIO.write(renderedImage, "png", file);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 
     private MenuItem[] getRecentFiles(String path) {
