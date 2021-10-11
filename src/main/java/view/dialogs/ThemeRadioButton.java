e61e2e385b4e836c593aee686555474637020bdb
Néo Almeida <neo.almeida.etu@univ-lille.fr>
2021-10-11 03:03:39 +0200

    Added graphic for each themes

--- src/main/java/view/dialogs/ThemeRadioButton.java
+++ src/main/java/view/dialogs/ThemeRadioButton.java
@@ -1,7 +1,14 @@
 package view.dialogs;
 
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.RadioMenuItem;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 
 /**
@@ -19,13 +26,47 @@ import javafx.scene.control.ToggleGroup;
  * @version %I%, %G%
  */
 public class ThemeRadioButton extends RadioMenuItem {
+
+    private Canvas tmp;
+
     public ThemeRadioButton(String color, ToggleGroup group) {
         super(color);
 
+        try {
+            setGraphic(getCurrentColor(color));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            setGraphic(new Rectangle(15,15, Color.RED));
+        }
+
+        //tmp = new Canvas()
+
         /* ID IS FOR CUSTOM CSS, THEME WONT WORK WITH THIS !!
            REMEMBER TO CHANGE ID IF YOU DECIDE TO CHANGE NAME OF FILES.CSS */
-        this.setId("javafxTestCSS" + color);
+        this.setId("Theme" + color);
 
         this.setToggleGroup(group);
     }
+
+    public Rectangle getCurrentColor(String color) throws FileNotFoundException {
+        //Rectangle tmp = new Rectangle(Color.web())
+        File f = new File("src/main/resources/css/theme/Theme" + color + ".css");
+        System.out.println("FILE =>"+f);
+        Scanner sc = new Scanner(f);
+
+        String rectColor = "white";
+        while(sc.hasNext()) {
+            String tmp = sc.nextLine();
+            if(tmp.contains("-fx-base:")) {
+                System.out.println(tmp.length());
+                rectColor = tmp.substring(14,tmp.length()-1);
+                break;
+            }
+        }
+        super.setStyle("-fx-base: "+ rectColor + ";");
+        if(rectColor.length() > 7) rectColor = "#ffffff";
+        return new Rectangle(15,15, Color.web(rectColor));
+    }
+
+
 }
