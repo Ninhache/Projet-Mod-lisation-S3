25f89060b0512519e2b6e5354383df6207a3396c
Simon Lagneau <simon.lagneau.etu@univ-lille.fr>
2021-10-14 18:52:40 +0200

    Updated Reader, now able to make multiple models

--- src/main/java/model/Reader.java
+++ src/main/java/model/Reader.java
@@ -15,7 +15,7 @@ public class Reader {
 	private String fileName,authorName;
 	private File file;
 	private final String END_HEADER = "end_header";
-	private int nbVertex,nbFaces,headerLength,vertexLength;
+	private int nbVertex,nbFaces,vertexLength;
 	private ArrayList<Vertex> points;
 	private ArrayList<Face> faces;
 
@@ -27,8 +27,8 @@ public class Reader {
 	 * @param file file to read
 	 */
 	public Reader(File file){
-		this.fileName = file.getName();
 		this.file = file;
+		this.fileName = file.getName();
 		points = new ArrayList<Vertex>();
 		faces = new ArrayList<Face>();
 	}
@@ -37,6 +37,8 @@ public class Reader {
 		this(new File("src/main/resources/"+fileName+".ply"));
 	}
 
+    
+    
 	public void readHeader(BufferedReader br) {
 		
 		try {
@@ -46,27 +48,24 @@ public class Reader {
 			
 			while((line=br.readLine())!=null && !line.equals(END_HEADER)) {
 				
-				headerLength++;
 				if(line.contains("element face ")) {
-					
 					nbFaces=Integer.parseInt(String.valueOf(line.charAt(line.length()-1)));
-					
-				} else if (line.contains("element vertex ")) {
-					
-					nbVertex=Integer.parseInt(String.valueOf(line.charAt(line.length()-1)));
-					
-				} else if (line.contains("comment created by "))authorName = line.substring(line.lastIndexOf(" ")+1);
-					sb.append(line);
-					sb.append("\n");
 				
-			}
+				} else if (line.contains("element vertex ")) {
+					nbVertex=Integer.parseInt(String.valueOf(line.charAt(line.length()-1)));
+				
+				} else if (line.contains("comment created by "))
+					authorName = line.substring(line.lastIndexOf(" ")+1);
 
+				sb.append(line);
+				sb.append("\n");
+			}
 			System.out.printf("contenu Header :\n\n%s________________________________________________________\n", sb.toString());
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		
 		} catch (IOException e) {
-			
 			e.printStackTrace();
 		
 		}
@@ -102,36 +101,40 @@ public class Reader {
 		try {
 			
 			String line = br.readLine();
-			String[] info = line.split(" ");
+			String[] splittedLine = line.split(" ");
 
 			StringBuilder sb = new StringBuilder();
 			
 			boolean testIfVertex = false;
-			if(info.length == 3) testIfVertex = true;			
+			if(splittedLine.length == 3) testIfVertex = true;			
 			int i = 1;
 			while(testIfVertex) {
 
 				vertexLength++;
 				nbVertexLines++;		
-				collectVertexInfo(info);
+				collectVertexInfo(splittedLine);
 				
 				sb.append(line);
 				sb.append("\n");
 				
 				br.mark(vertexLength);
 				line=br.readLine();
-				info = line.split(" ");
-				if( info.length != 3 ) testIfVertex = false;
-				
+				splittedLine = line.split(" ");
+				if( splittedLine.length != 3 )
+					testIfVertex = false;
 			}
 		
-			if (nbVertexLines != nbVertex) throw new Exception("element Vertex = " + nbVertex + " && nombre de Lignes de Vertex = " + (i-1) + "");
+			if (nbVertexLines != nbVertex)
+				throw new Exception("element Vertex = " + nbVertex + " && nombre de Lignes de Vertex = " + (i-1) + "");
 
 			System.out.printf("contenu Vertex :\n\n%s________________________________________________________\n", sb.toString());
+		
 		} catch (IOException e) {
 			e.printStackTrace();
+			
 		} catch (Exception e) {
 			e.printStackTrace();
+			
 		}	
 	}
 	
@@ -141,7 +144,7 @@ public class Reader {
 		 try {
 			 
 			 String line = null;
-			 String[] info;
+			 String[] splittedLine;
 			 
 			 StringBuilder sb = new StringBuilder();
 			 br.reset();
@@ -151,15 +154,15 @@ public class Reader {
 			 
 			 while((line=br.readLine())!=null && testIfFace) {
 				
-				info = line.split(" ");		
-				collectFaceInfo(info);
+				splittedLine = line.split(" ");		
+				collectFaceInfo(splittedLine);
 				nbFaceLines++;
 				i++;
 				
 				sb.append(line);
 				sb.append("\n");
 				
-				if( info.length != 4 ) testIfFace= false;
+				if( splittedLine.length != 4 ) testIfFace= false;
 				
 			 }
 			
@@ -168,6 +171,7 @@ public class Reader {
 			 System.out.printf("contenu Faces :\n\n%s\nNombre d'itérations : %d\n________________________________________________________\n\n", sb.toString(), i-1);
 		 } catch(Exception e){
 			 e.printStackTrace();
+		 
 		 }
 	}
 	
@@ -187,7 +191,12 @@ public class Reader {
 	public File getFile() {
 		return file;
 	}
-
+	public void setFile(File file) {
+		this.file = file;
+	}
+	public void setFile(String fileName) {
+		this.file = new File(fileName);
+	}
 	public String getAuthorName() {
 		return "Created by " + authorName;
 	}
@@ -195,10 +204,12 @@ public class Reader {
 	public static void main(String args[]) throws FileNotFoundException{
 		File test = new File("D:\\eclipse-workspace\\projetmodeg5\\src\\main\\resources\\test.ply");
 		Reader reader = new Reader(test);
-		FileReader fr = new FileReader(reader.getFile());
-		BufferedReader br = new BufferedReader(fr);
-		
 		reader.readPly();
 		System.out.println(reader.getAuthorName());
+		reader.setFile("D:\\eclipse-workspace\\projetmodeg5\\src\\main\\resources\\test3.ply");
+		reader.readPly();
+		System.out.println(reader.getAuthorName());
+		reader.setFile("D:\\eclipse-workspace\\projetmodeg5\\src\\main\\resources\\test3.ply");
+		reader.readPly();
 	}
 }
