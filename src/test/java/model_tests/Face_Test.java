d41ebed673d8a16caeafdcff1cb534f89927c6e0
Simon Lagneau <simon.lagneau.etu@univ-lille.fr>
2021-11-02 16:53:58 +0100

    Added Face-Test class

--- src/test/java/model_tests/Face_Test.java
+++ src/test/java/model_tests/Face_Test.java
@@ -5,10 +5,12 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+
 class Face_Test {
 
 	@BeforeEach
 	void setUp() throws Exception {
+
 	}
 
 	@Test
